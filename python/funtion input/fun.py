43d860379c956a3b382a850f8a691907bc9b9eca
aayushkumar1679 <ayushanandexel24@gmail.com>
2025-11-30 11:52:14 +0530

    function added with user input

---
+++ python/funtion input/fun.py
@@ -0,0 +1,12 @@
+# def life_in_weeks(x):
+#     years_left = 90 - x
+#     left = years_left * 52
+#     print(f"You have {left} weeks left.")
+#
+#
+# life_in_weeks(12)
+
+def printwith (name , location):
+    print(f"hello {name} from {location}")
+
+printwith("ayush","india")
--- python/hangman/project.py
+++ python/hangman/project.py
@@ -1,25 +1,103 @@
 import random
 
-w_list = [
-    "ayush",
-    "aman",
-    "rohit",
-    "suman",
-    "santy",
+hangman_stages = [
+    """
+       +---+
+       |   |
+           |
+           |
+           |
+           |
+    =========
+    """,
+    """
+       +---+
+       |   |
+       O   |
+           |
+           |
+           |
+    =========
+    """,
+    """
+       +---+
+       |   |
+       O   |
+       |   |
+           |
+           |
+    =========
+    """,
+    """
+       +---+
+       |   |
+       O   |
+      /|   |
+           |
+           |
+    =========
+    """,
+    """
+       +---+
+       |   |
+       O   |
+      /|\\  |
+           |
+           |
+    =========
+    """,
+    """
+       +---+
+       |   |
+       O   |
+      /|\\  |
+      /    |
+           |
+    =========
+    """,
+    """
+       +---+
+       |   |
+       O   |
+      /|\\  |
+      / \\  |
+           |
+    =========
+    """,
 ]
+
+w_list = ["ayush", "aman", "rohit", "suman", "santy"]
 randomword = random.choice(w_list)
 
+hangman_stages.reverse()
 print(randomword)
 
+game_over = False
+correct_letter = []
+lives = len(hangman_stages) - 1
 
-rightg = ""
+while not game_over:
+    guess = input("guess a letter \n").lower()
+    display = ""
 
-
-while not rightg == randomword:
-    guesss = input("guess the letter?\n").lower()
-    for l in randomword:
-        if l == randomword:
-            rightg += guesss
+    for letter in randomword:
+        if letter in correct_letter or letter == guess:
+            display += letter
         else:
-            rightg += "_"
-print(rightg)
+            display += "_"
+
+    if guess in randomword:
+        correct_letter.append(guess)
+    else:
+        print(hangman_stages[lives])
+        lives -= 1
+        if lives < 0:
+            game_over = True
+            print("you lose")
+            break
+
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("you win")
